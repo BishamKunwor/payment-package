c96f876d1662a305673e0df282d4118d7318dc01
Bisham Kunwor <novelian.nova@gmail.com>
2023-08-25 12:23:54 +0545

    Updated to Version 1 Draft 1

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,11 @@
 import { EsewaPayment } from "./Esewa";
 import { KhaltiPayment } from "./Khalti";
 
-const esewaPayment = new EsewaPayment();
+// const esewaPayment = new EsewaPayment();
+// esewaPayment.initiate({
+//   amount: 500,
+//   processId: "Bisham Test Suite",
+//   totalAmount: 500,
+// });
 
 export { EsewaPayment };
